eef6be4e924a827b51990bdaaeb25124f1889418
Krzysztof <km.2001@o2.pl>
2020-11-15 20:37:57 +0100

    v0.2 release

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='messenger-counter',
-    version='0.1',
-    author="Krzysztof Mizgała",
-    author_email="KMChris007@gmail.com",
-    description="Package for counting messages from Facebook Messenger",
+    version='0.2',
+    author='Krzysztof Mizgała',
+    author_email='KMChris007@gmail.com',
+    description='Package for counting messages from Facebook Messenger',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/KMChris/messenger-counter",
+    long_description_content_type='text/markdown',
+    url='https://github.com/KMChris/messenger-counter',
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
     ],
     python_requires='>=3.5',
+    install_requires=['pandas', 'matplotlib'],
+    py_modules=['MessengerCounter'],
 )
