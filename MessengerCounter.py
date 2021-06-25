26df0254167397ea45a30104302d587033b64b2f
Krzysztof Mizgała <km.2001@o2.pl>
2021-06-25 16:49:17 +0200

    Code refactor

--- MessengerCounter.py
+++ MessengerCounter.py
@@ -10,6 +10,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+# Getting data
+
 def set_source(filename):
     filename = f'file:///{filename}' if filename[1] == ':' \
         else (f'file:./{filename}' if filename.endswith('.zip') else f'file:./{filename}.zip')
@@ -25,14 +27,14 @@ def get_data(conversation=None, chars=False, user=False):
         if user:
             data = pd.DataFrame(data).fillna(0).astype('int')
             for key in data.index:
-                if key.startswith(' '.join(conversation)):
+                if key.lower().startswith(conversation):
                     return data, key
             else:
                 logging.error('Conversation not found.')
                 return None
         if conversation is not None:
             for key in data.keys():
-                if key.startswith(conversation):
+                if key.lower().startswith(conversation):
                     return data, key
             else:
                 logging.error('Conversation not found.')
@@ -42,6 +44,8 @@ def get_data(conversation=None, chars=False, user=False):
     except FileNotFoundError:
         logging.error('Characters not counted.' if chars else 'Messages not counted.')
 
+
+# Counting messages and characters
 
 def count_messages():
     namelist = source.namelist()
@@ -61,7 +65,6 @@ def count_messages():
         total[sender]['total'] = sum(messages.values())
     with open('messages.json', 'w', encoding='utf-8') as output:
         json.dump(total, output, ensure_ascii=False)
-
 
 def count_characters():
     namelist = source.namelist()
@@ -83,7 +86,6 @@ def count_characters():
     with open('messages_chars.json', 'w', encoding='utf-8') as output:
         json.dump(total, output, ensure_ascii=False)
 
-
 def count(chars=False):
     if chars:
         count_characters()
@@ -91,130 +93,7 @@ def count(chars=False):
         count_messages()
 
 
-def messages_statistics(data_source):
-    data_source = pd.DataFrame(data_source).fillna(0).astype('int')
-    pd.set_option('display.max_rows', None)
-    total_values = data_source.loc['total'].sort_values(ascending=False)
-    print(total_values)
-    print(total_values.describe())
-    total_values = total_values.sort_values()
-    plt.rcdefaults()
-    plt.barh(total_values.index.astype(str).str[:10][-20:], total_values.iloc[-20:])
-    plt.show()
-
-
-def conversation_statistics(data_source, conversation):
-    data_source = pd.DataFrame(data_source)
-    data_source = data_source.loc[:, conversation]
-    data_source = data_source[data_source > 0].sort_values(ascending=False).astype('int')
-    pd.set_option('display.max_rows', None)
-    print(data_source)
-
-
-def user_statistics(data_source, user_name):
-    data_source = data_source.loc[user_name]
-    data_source = data_source[data_source > 0].sort_values(ascending=False)
-    data_source.index = data_source.index.map(lambda x: x.split('_')[0][:30])
-    pd.set_option('display.max_rows', None)
-    print(user_name, 'statistics:')
-    print(data_source)
-
-
-def interval_count(inbox_name, function, delta=0.0):
-    messages, i = collections.Counter(), 0
-    while True:
-        try:
-            i += 1
-            messages += collections.Counter(function(pd.to_datetime(pd.DataFrame(json.loads(
-                source.open('messages/inbox/' + inbox_name + '/message_' + str(i) + '.json').read())[
-                            'messages']).iloc[:, 1], unit='ms').dt.tz_localize('UTC').dt.tz_convert(
-                            'Europe/Warsaw').add(pd.Timedelta(hours=-delta))))
-        except KeyError:
-            break
-    return messages
-
-
-def interval_plot(messages):
-    messages = pd.Series(messages).sort_index()
-    print(messages.describe())
-    plt.bar(messages.index, messages)
-    # plt.savefig('messages.pdf')
-    plt.show()
-
-
-def hours_plot(messages, delta):
-    messages = pd.DataFrame(messages, index=[0])
-    print(messages.iloc[0].describe())
-    plt.bar(messages.columns, messages.iloc[0])
-    plt.xticks(list(range(24)), [f'{x % 24}:{int(abs((delta - int(delta)) * 60)):02}'
-                                 for x in range(-(-math.floor(delta) % 24),
-                                 math.floor(delta) % 24 if math.floor(delta) % 24 != 0 else 24)], rotation=90)
-    plt.xlim(-1, 24)
-    # plt.savefig('messages.pdf')
-    plt.show()
-
-
-def hours_conversation(conversation, delta=0.0):
-    hours_plot(interval_count(conversation, lambda x: x.dt.hour, delta), delta)
-
-
-def hours_chats(delta=0.0):
-    messages = collections.Counter()
-    for sender in {x.split('/')[2] for x in source.namelist()
-                   if (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
-        messages += interval_count(sender, lambda x: x.dt.hour, delta)
-    hours_plot(messages, delta)
-
-
-def daily_conversation(conversation, delta=0.0):
-    interval_plot(interval_count(conversation, lambda x: x.dt.date, delta))
-
-
-def daily_chats(delta=0.0):
-    messages = collections.Counter()
-    for sender in {x.split('/')[2] for x in source.namelist() if
-                   (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
-        messages += interval_count(sender, lambda x: x.dt.date, delta)
-    interval_plot(messages)
-
-
-def monthly_conversation(conversation):  # TODO not working charts for monthly
-    interval_plot(interval_count(conversation, lambda x: x.dt.to_period("M").astype('datetime64[ns]')))
-
-
-def monthly_chats():
-    messages = collections.Counter()
-    for sender in {x.split('/')[2] for x in source.namelist() if
-                   (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
-        messages += interval_count(sender, lambda x: x.dt.to_period("M").astype('datetime64[ns]'))
-    interval_plot(messages)
-
-
-def yearly_conversation(conversation):
-    interval_plot(interval_count(conversation, lambda x: x.dt.year))
-
-
-def yearly_chats():
-    messages = collections.Counter()
-    for sender in {x.split('/')[2] for x in source.namelist()
-                   if (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
-        messages += interval_count(sender, lambda x: x.dt.year)
-    messages = pd.DataFrame(messages, index=[0])
-    print(messages.iloc[0].describe())
-    plt.bar(messages.columns, messages.iloc[0])
-    # plt.savefig('messages.pdf')
-    plt.show()
-
-
-def characters_statistics(data_source):
-    data_source = pd.DataFrame(data_source)
-    data_source['total'] = data_source.sum(axis=1)
-    data_source = data_source.iloc[:, -1]
-    data_source = data_source.sort_values(ascending=False).astype('int')
-    pd.set_option('display.max_rows', None)
-    print(data_source)
-    print(f'Total characters: {data_source.sum()}')
-
+# Statistics
 
 def statistics(data_source, conversation=None, chars=False):
     if conversation is None:
@@ -231,12 +110,99 @@ def statistics(data_source, conversation=None, chars=False):
             print(conversation)
             conversation_statistics(data_source, conversation)
 
+def messages_statistics(data_source):
+    data_source = pd.DataFrame(data_source).fillna(0).astype('int')
+    pd.set_option('display.max_rows', None)
+    total_values = data_source.loc['total'].sort_values(ascending=False)
+    print(total_values)
+    print(total_values.describe())
+    total_values = total_values.sort_values()
+    plt.rcdefaults()
+    plt.barh(total_values.index.astype(str).str[:10][-20:], total_values.iloc[-20:])
+    plt.show()
+
+def conversation_statistics(data_source, conversation):
+    data_source = pd.DataFrame(data_source)
+    data_source = data_source.loc[:, conversation]
+    data_source = data_source[data_source > 0].sort_values(ascending=False).astype('int')
+    pd.set_option('display.max_rows', None)
+    print(data_source)
+
+def characters_statistics(data_source):
+    data_source = pd.DataFrame(data_source)
+    data_source['total'] = data_source.sum(axis=1)
+    data_source = data_source.iloc[:, -1]
+    data_source = data_source.sort_values(ascending=False).astype('int')
+    pd.set_option('display.max_rows', None)
+    print(data_source)
+    print(f'Total characters: {data_source.sum()}')
+
+
+# User statistics
+
+def user_statistics(data_source, user_name):
+    data_source = data_source.loc[user_name]
+    data_source = data_source[data_source > 0].sort_values(ascending=False)
+    data_source.index = data_source.index.map(lambda x: x.split('_')[0][:30])
+    pd.set_option('display.max_rows', None)
+    print(user_name, 'statistics:')
+    print(data_source)
+
+
+# Intervals
+
+def interval_count(inbox_name, function, delta=0.0):
+    messages, i = collections.Counter(), 0
+    while True:
+        try:
+            i += 1
+            messages += collections.Counter(function(pd.to_datetime(pd.DataFrame(json.loads(
+                source.open('messages/inbox/' + inbox_name + '/message_' + str(i) + '.json').read())[
+                            'messages']).iloc[:, 1], unit='ms').dt.tz_localize('UTC').dt.tz_convert(
+                            'Europe/Warsaw').add(pd.Timedelta(hours=-delta))))
+        except KeyError:
+            break
+    return messages
+
+def interval_plot(messages):
+    messages = pd.Series(messages).sort_index()
+    print(messages.describe())
+    plt.bar(messages.index, messages)
+    # plt.savefig('messages.pdf')
+    plt.show()
+
+
+# Hours
+
 def hours(difference, conversation=None):
     if conversation is None:
-        daily_chats(difference)
+        hours_chats(difference)
     else:
-        daily_conversation(conversation, difference)
+        hours_conversation(conversation, difference)
 
+def hours_conversation(conversation, delta=0.0):
+    hours_plot(interval_count(conversation, lambda x: x.dt.hour, delta), delta)
+
+def hours_chats(delta=0.0):
+    messages = collections.Counter()
+    for sender in {x.split('/')[2] for x in source.namelist()
+                   if (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
+        messages += interval_count(sender, lambda x: x.dt.hour, delta)
+    hours_plot(messages, delta)
+
+def hours_plot(messages, delta):
+    messages = pd.DataFrame(messages, index=[0])
+    print(messages.iloc[0].describe())
+    plt.bar(messages.columns, messages.iloc[0])
+    plt.xticks(list(range(24)), [f'{x % 24}:{int(abs((delta - int(delta)) * 60)):02}'
+                                 for x in range(-(-math.floor(delta) % 24),
+                                 math.floor(delta) % 24 if math.floor(delta) % 24 != 0 else 24)], rotation=90)
+    plt.xlim(-1, 24)
+    # plt.savefig('messages.pdf')
+    plt.show()
+
+
+# Daily
 
 def daily(difference, conversation=None):
     if conversation is None:
@@ -244,6 +210,31 @@ def daily(difference, conversation=None):
     else:
         daily_conversation(conversation, difference)
 
+def daily_conversation(conversation, delta=0.0):
+    interval_plot(interval_count(conversation, lambda x: x.dt.date, delta))
+
+def daily_chats(delta=0.0):
+    messages = collections.Counter()
+    for sender in {x.split('/')[2] for x in source.namelist() if
+                   (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
+        messages += interval_count(sender, lambda x: x.dt.date, delta)
+    interval_plot(messages)
+
+
+# Monthly (not working)
+
+def monthly_conversation(conversation):  # TODO not working charts for monthly
+    interval_plot(interval_count(conversation, lambda x: x.dt.to_period("M").astype('datetime64[ns]')))
+
+def monthly_chats():
+    messages = collections.Counter()
+    for sender in {x.split('/')[2] for x in source.namelist() if
+                   (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
+        messages += interval_count(sender, lambda x: x.dt.to_period("M").astype('datetime64[ns]'))
+    interval_plot(messages)
+
+
+# Yearly
 
 def yearly(conversation=None):
     if conversation is None:
@@ -251,8 +242,22 @@ def yearly(conversation=None):
     else:
         yearly_conversation(conversation)
 
+def yearly_conversation(conversation):
+    interval_plot(interval_count(conversation, lambda x: x.dt.year))
 
-def characters_conversation_statistics(data_source, conversation):  # TODO characters conversation statistics
+def yearly_chats():
+    messages = collections.Counter()
+    for sender in {x.split('/')[2] for x in source.namelist()
+                   if (x.endswith('/') and x.startswith('messages/inbox/') and x != 'messages/inbox/')}:
+        messages += interval_count(sender, lambda x: x.dt.year)
+    messages = pd.DataFrame(messages, index=[0])
+    print(messages.iloc[0].describe())
+    plt.bar(messages.columns, messages.iloc[0])
+    # plt.savefig('messages.pdf')
+    plt.show()
+
+# TODO characters conversation statistics
+def characters_conversation_statistics(data_source, conversation):
     print()
 
 
--- mc.py
+++ mc.py
@@ -14,6 +14,8 @@ if __name__ == '__main__':
     parser_count.add_argument('file',
         help='path to .zip file downloaded from Facebook, '
              'add -c argument to count chars')
+    parser_count.add_argument('-c', '--chars', action='store_true',
+        help='refer to characters instead of messages')
 
     parser_stats = subparsers.add_parser('stats',
         help='displays statistics for counted messages, '
@@ -21,11 +23,17 @@ if __name__ == '__main__':
              'use -c for character statistics')
     parser_stats.add_argument('conversation', nargs='?', type=str, const=None,
         help='name of the conversation')
+    parser_stats.add_argument('-c', '--chars', action='store_true',
+        help='refer to characters instead of messages')
 
     parser_user = subparsers.add_parser('user',
         help='detailed statistics for specific user')
-    parser_user.add_argument('conversation', nargs='+', type=list,
-        help='name of the conversation with specific user')
+    parser_user.add_argument('conversation', type=str,
+        help='name of a user')
+    parser_user.add_argument('-c', '--chars', action='store_true',
+        help='refer to characters instead of messages')
+    parser_user.add_argument('-d', '--difference', type=float, default=0.0,
+        help='time shift in hours to show statistics differently')
 
     parser_yearly = subparsers.add_parser('yearly',
         help='number of messages per year')
@@ -36,15 +44,14 @@ if __name__ == '__main__':
         help='average number of messages by day of the week')
     parser_daily.add_argument('conversation', nargs='?', type=str, const=None,
         help='name of the conversation with specific user')
+    parser_daily.add_argument('-d', '--difference', type=float, default=0.0,
+        help='time shift in hours to show statistics differently')
 
     parser_hours = subparsers.add_parser('hours',
         help='average number of messages by hour')
     parser_hours.add_argument('conversation', nargs='?', type=str, const=None,
         help='name of the conversation with specific user')
-
-    parser.add_argument('-c', '--chars', action='store_true',
-        help='refer to characters instead of messages')
-    parser.add_argument('-d', '--difference', type=float, default=0.0,
+    parser_hours.add_argument('-d', '--difference', type=float, default=0.0,
         help='time shift in hours to show statistics differently')
 
     args = parser.parse_args()
@@ -63,7 +70,7 @@ if __name__ == '__main__':
         )
     elif args.command == 'user':
         MessengerCounter.user_statistics(
-            MessengerCounter.get_data(user=True)
+            *MessengerCounter.get_data(args.conversation, user=True)
         )
     elif args.command == 'yearly':
         MessengerCounter.yearly(args.conversation)
